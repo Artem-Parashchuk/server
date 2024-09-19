b25dfe17fb7e145d67a4c887c33fc03a17695429
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-09-19 18:19:00 +0300

    add DB_HOST to process.env

--- initMongoDB.js
+++ initMongoDB.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
+const {DB_HOST} = process.env
+ 
 export const initMongoDB = async () => {
     try{
-        const DB_HOST = "mongodb+srv://Artem:12345678qwerty@eliftechcluster.r8w8y.mongodb.net/eliftech?retryWrites=true&w=majority&appName=EliftechCluster"
         await mongoose.connect(DB_HOST)
         console.log("Successfully connection to MongoDB")
 
