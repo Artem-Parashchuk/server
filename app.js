dbd8a0be05871e6a65c3ae30057483cd20ba8682
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-09-19 17:59:25 +0300

    create conekt

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-import express from "express";
-import cors from 'cors'
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
+import { initMongoDB } from "./initMongoDB.js"
+import { startServer } from "./startServer.js"
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Сервер запущено на порту ${PORT}`))
+const bootstrap = async () => {
+    await initMongoDB()
+    startServer()
+}
+bootstrap()
