3cef30226d45123ec85b6947bb2b559dcf97c04e
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-09-20 15:27:16 +0300

    added the ability to receive users by id

--- services/eventService.js
+++ services/eventService.js
@@ -1,14 +1,16 @@
 import { Event } from "../models/Event.js";
 import { User } from "../models/Users.js";
 
-export const getEvents = () => Event.find()
+export const getEvents = () => Event.find();
 
 export const getUser = async () => {
-    try {
-      const people = await User.find(); // Запит до колекції registerPeople
-      return people;
-    } catch (error) {
-      console.error(`Error fetching registered people: ${error.message}`); // Логування помилки
-      throw error; // Повернення помилки
-    }
-  };
+  try {
+    const people = await User.find();
+    return people;
+  } catch (error) {
+    console.error(`Error fetching registered people: ${error.message}`);
+    throw error;
+  }
+};
+
+export const getUserById = (_id) => User.findOne(_id);
--- startServer.js
+++ startServer.js
@@ -1,37 +1,52 @@
 import express from "express";
-import cors from 'cors'
-import dotenv from 'dotenv'
-import { getEvents, getUser } from "./services/eventService.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import { getEvents, getUser, getUserById } from "./services/eventService.js";
 
-dotenv.config()
+dotenv.config();
 
 export const startServer = () => {
+  const app = express();
 
-    const app = express()
-    
-    app.use(cors())
+  app.use(cors());
 
+  app.get("/api/users", async (req, res) => {
+    const data = await getUser();
+    res.status(200).json({
+      status: 200,
+      message: "Register user successfully",
+      data,
+    });
+  });
 
-    app.get("/api/users", async (req, res) => {
-        const data = await getUser()
-        res.status(200).json({
-            status: 200,
-            message: "Register people successfully",
-            data
-        })
-    })
+  app.get("/api/users/:id", async (req, res) => {
+    const { is } = req.params;
 
-    app.get("/api/events", async (req, res) => {
-        const data = await getEvents()
-        res.status(200).json({
-            status: 200,
-            message: "Events get successfully",
-            data
-        })
-    })
+    const data = await getUserById(id);
 
-    
-    
-    const PORT = process.env.PORT 
-    app.listen(PORT, () => console.log(`Сервер запущено на порту ${PORT}`))
-}
+    if (!data) {
+      return res.status(404).json({
+        status: 404,
+        message: `User with id=${id} not found`,
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: "Register user by ID get successfully",
+      data,
+    });
+  });
+
+  app.get("/api/events", async (req, res) => {
+    const data = await getEvents();
+    res.status(200).json({
+      status: 200,
+      message: "Events get successfully",
+      data,
+    });
+  });
+
+  const PORT = process.env.PORT;
+  app.listen(PORT, () => console.log(`Сервер запущено на порту ${PORT}`));
+};
